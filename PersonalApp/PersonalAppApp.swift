4beb4168af23e4cea882838801b64ab54f32ac75
ennomueller@online.de <ennomueller@online.de>
2023-01-04 17:20:13 +0100

    Initial Commit

---
+++ PersonalApp/PersonalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAppApp.swift
+//  PersonalApp
+//
+//  Created by Paul Wagner on 04.01.23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
